83e8936442231cc39e2f24ef977582f9b18e8ce4
itechdom <idea@itechdom.com>
2015-07-08 14:27:14 -0600

    added vdom rendering

--- app/client/components/todo/view.js
+++ app/client/components/todo/view.js
@@ -3,13 +3,34 @@ var $ = require('jquery');
 var Rx = require('rx');
 var actions = require('./actions.js');
 var model = require('./model.js')
+var h = require('virtual-dom/h');
+var diff = require('virtual-dom/diff');
+var patch = require('virtual-dom/patch');
+var createElement = require('virtual-dom/create-element');
+
+
 //default actions
 
 class todoView{
 
 	constructor() {
+
 		this.actions = {
 			templateLoaded$: Rx.Observable.fromPromise($.get('./app/client/components/todo/todo.html'))
+		}
+
+		this.template = "<todo></todo>";
+
+		this.render = function(count){
+			return h('div', {
+				style: {
+					textAlign: 'center',
+			    lineHeight: (100 + count) + 'px',
+			    border: '1px solid red',
+			    width: (100 + count) + 'px',
+			    height: (100 + count) + 'px'
+				}
+			}, [String(count)]);
 		}
 
 	}
@@ -17,19 +38,27 @@ class todoView{
 	wire(){
 
 		//I can combine latest here and send back the template with its data
-		this.actions.templateLoaded$.subscribe(function(data){
+		this.actions.templateLoaded$.subscribe((data) =>{
 			//I can test the type of the data here before diswireing it
-			console.log(data);
-			var elem = $("<h1>"+data+"</h1>");
-			$('body').html(elem);
+			var count = 0;
+			var vtree = this.render(count);
+			var nodeTree = createElement(vtree);
+			$('body').append(nodeTree);
+			//create the scene graph here
 		});
+
 		actions.insertTodo$.subscribe(function(){
 			//do something here to deal with when the todo is inserting
+			//call the diff patch
+
 		})
 
-		model.actions.dataChanged$.subscribe(function(data){
-			console.log(data,"data have changed from view");
-			$('body').html(data.toString());
+		model.actions.dataChanged$.subscribe((data) => {
+			//call vdom diff and rerender the dom?
+			var count = 1;
+			var vtree = this.render(count);
+			var nodeTree = createElement(vtree);
+			$('body').html(nodeTree);
 		})
 
 	}
